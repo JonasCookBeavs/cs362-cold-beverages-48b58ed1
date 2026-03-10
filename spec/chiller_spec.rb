2aaf7b0c2bb4211105ace2d3964087b32abb8aad
JonasCookBeavs <jonascook04@gmail.com>
2026-03-10 16:52:31 -0700

    Test chiller file

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,5 +1,29 @@
 require_relative '../lib/chiller'
 
 describe 'A chiller' do
+  
+  it 'has a capacity and temperature' do
+    chiller = Chiller.new
+    expect(chiller.capacity).to eq(100)
+    expect(chiller.temperature).to eq(70) # 70 is default temperature
+  end
+
+  it 'initially has full capacity' do
+    chiller = Chiller.new(capacity=100)
+    expect(chiller.remaining_capacity).to eq(100)
+  end
+
+  it 'has less capacity after item is added' do
+    chiller = Chiller.new(capacity=100)
+    item = Item.new(name='FAKE', volume=50)
+    chiller.add(item)
+    expect(chiller.remaining_capacity).to eq(50)
+  end
+
+  it 'can set level' do # Not really sure what "level" means in this context, but sure
+    chiller = Chiller.new
+    chiller.set_level(1)
+    expect(chiller.temperature).to eq(65) # 65 = Default temp - level * 10
+  end
 
 end
