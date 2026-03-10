79d5e85523378ddbc74a0ccb4e50cd3ff3039e3c
JonasCookBeavs <jonascook04@gmail.com>
2026-03-10 16:23:08 -0700

    Test water_dispenser file

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,19 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has a reservoir' do
+    reservoir = WaterReservoir.new
+    water_dispenser = WaterDispenser.new(reservoir)
+    expect(water_dispenser.reservoir).to eq(reservoir)
+  end
+
+  it 'dispenses from reservoir' do
+    vessel = Vessel.new(volume=100)
+    reservoir = WaterReservoir.new(vessel.volume, vessel.volume)
+    water_dispenser = WaterDispenser.new(reservoir)
+    
+    water_dispenser.dispense(vessel)
+    expect(reservoir).to be_empty
+  end
+  
 end
